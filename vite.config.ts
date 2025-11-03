2d8f311e9d2e0922e15d2e8ea128dae9f440481b
Segu_g <segusegment@gmail.com>
2025-11-03 23:41:37 +0900

    setup paging options

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+
+const rootDir = resolve(__dirname, "./src/pages") 
 
 // https://vite.dev/config/
 export default defineConfig({
+  root: rootDir,
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: {
+        main: resolve(rootDir, "./index.html")
+      },
+      output: {
+        entryFileNames: `js/[name]/bundle.js`
+      }
+    }
+  },
   plugins: [react()],
 })
